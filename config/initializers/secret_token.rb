23ed16f0c5299ddf26bb6ff944007b14cfa4f00c
frank <frank@octopusbusinessenterprise.com>
2012-01-12 14:39:09 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coreengine::Application.config.secret_token = '2b9c89182396ed4a1cffc2f9229144a9bc251c27514a6b72f31b11add78bb344fca47d17c65f7812fa3800a9f0a7da88ed17c2b03d985bd5eea8fe8ba0795d9f'
